cb85d09cce0da2f124773d4b3d6ecd307fdf9454
Kerim Yıldız <kerim.dnzl20@gmail.com>
2026-02-06 14:55:52 +0300

    Create script.js
    İlk JavaScript kodu eklendi.

---
+++ script.js
@@ -0,0 +1,9 @@
+let todos = [];
+
+function addTodo(todo) {
+    todos.push(todo);
+    console.log("Yapılacak eklendi:", todo);
+}
+
+addTodo("JavaScript çalış");
+addTodo("GitHub projesi hazırla");
